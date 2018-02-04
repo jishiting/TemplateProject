ae1390749c120dd729b9ca920b9c6a6e902eed2e
jishiting <312683897@qq.com>
2018-02-05 00:33:04 +0800

    refactor the param name

--- src/main/java/QueryFormatter1.java
+++ src/main/java/QueryFormatter1.java
@@ -2,9 +2,9 @@
  * Created by shitingji on 2018/2/5.
  */
 public class QueryFormatter1 {
-    public static String format(String something) {
-        if(something.startsWith("\"")&&something.endsWith("\"")) return something;
-        return something.toLowerCase()
+    public static String format(String value) {
+        if(value.startsWith("\"")&&value.endsWith("\"")) return value;
+        return value.toLowerCase()
                 .replace("-","\\-")
                 .replace("\\-","\\-");
     }
