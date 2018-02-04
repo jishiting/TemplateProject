f1a3fcebf6b1699607b112a83f540ac022c453d0
jishiting <312683897@qq.com>
2018-02-05 00:22:14 +0800

    add should transform param to lowercase

---
+++ src/main/java/QueryFormatter1.java
@@ -0,0 +1,8 @@
+/**
+ * Created by shitingji on 2018/2/5.
+ */
+public class QueryFormatter1 {
+    public static String format(String something) {
+        return something.toLowerCase();
+    }
+}
