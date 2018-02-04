fefdbbbe24c5ce390570f1ac6846f6ba3fa5a690
jishiting <312683897@qq.com>
2018-02-05 00:17:02 +0800

    delete class:
    ;

--- src/test/java/QueryFormatter1.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by shitingji on 2018/2/5.
- */
-public class QueryFormatter1 {
-    public static String format(String something) {
-        return something.toLowerCase();
-    }
-}
